7c5bfee0735398f47c7f90c4c96566971c042c0f
David Pereira <d_pereira@outlook.com.br>
2018-07-27 21:59:09 -0300

    Implementação do framework membership

---
+++ Financas/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace Financas.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(string login, string senha)
+        {
+            if(WebSecurity.Login(login, senha)){
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("login.Invalido", "Login ou senha incorretos");
+                return View("Index", "Movimentacao");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            return RedirectToAction("Index");
+        }
+    }
+}
